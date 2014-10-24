7c9aa8eea09f9a272c10827747140864334c45d7
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-10-24 22:35:53 +0900

    recommend the use of imagecreatetruecolor() instead of imagecreate()

--- images/fontcolor/color-icon.php
+++ images/fontcolor/color-icon.php
@@ -6,9 +6,10 @@ if ($isSameHost && $isColorSet) {
 
     list($red, $green, $blue) = str_split($_GET['color'], 2);
 
-    $img = imagecreate(16, 16);
+    $img = @imagecreatetruecolor(16, 16) or die("Cannot Initialize new GD image stream");
     imagefill($img, 0, 0, imagecolorallocate($img, hexdec($red), hexdec($green), hexdec($blue)));
     header('Content-type: image/png');
-    header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 60*60*24*365) . ' GMT');
+    header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 60*60*24) . ' GMT');
     imagepng($img);
+    imagedestroy($img);
 }
