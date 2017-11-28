59186aa53b64b53aa332342f695f8bb9a34d40ce
Zatalyz <zatalyz@liev.re>
2017-11-28 10:50:37 +0100

    Add french translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * French language file
+ */
+
+// custom language strings for the plugin
+$lang['fc_picker']  = 'Couleur de police';
+$lang['ff_picker']  = 'Famille de police';
+$lang['fs_picker']  = 'Taille de police';
+
+$lang['ff_serif']          = 'Serif';
+$lang['ff_sans-serif']     = 'Sans-serif';
+
+$lang['fs_xxs']            = 'Texte en minuscule ';
+$lang['fs_xxs_sample']     = 'Ceci est un texte écrit minuscule';
+$lang['fs_xs']             = 'Texte en très petit';
+$lang['fs_xs_sample']      = 'Ceci est un texte écrit très petit';
+$lang['fs_s']              = 'Texte en petit';
+$lang['fs_s_sample']       = 'Ceci est un texte écrit petit';
+$lang['fs_m']              = 'Texte en moyen (plus grand que par défaut)';
+$lang['fs_m_sample']       = 'Ceci est un texte à taille moyenne ';
+$lang['fs_l']              = 'Texte en gros';
+$lang['fs_l_sample']       = 'Ceci est un texte écrit gros';
+$lang['fs_xl']             = 'Texte en très gros';
+$lang['fs_xl_sample']      = 'Ceci est un texte écrit très gros';
+$lang['fs_xxl']            = 'Texte en énorme';
+$lang['fs_xxl_sample']     = 'Ceci est un texte écrit énorme';
+$lang['fs_smaller']        = 'Texte plus petit que par défaut';
+$lang['fs_smaller_sample'] = 'Ce texte est plus petit que le texte par défaut';
+$lang['fs_larger']         = 'Texte plus grand que par défaut';
+$lang['fs_larger_sampel']  = 'Ce texte est plus grand que le texte par défaut';
+
+$lang['odt_too_old']       = 'Nécessite un plugin d\'export ODT plus récent qui supporte la conversion des noms de couleurs css. Veuillez installer au moins la version 12102014. Cela n\'influence que l\'exportation ODT, le rendu XHTML ne sera pas affecté.';
+
